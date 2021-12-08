1f86271443e5738394a7d9464790548743f88846
Bohdan <52108700+Okashichan@users.noreply.github.com>
2021-12-08 17:10:12 +0200

    fix change mail issues

--- website/users.py
+++ website/users.py
@@ -26,6 +26,9 @@ def home():
             flash('Паролі не співпадають.', category='error')
         elif len(password2) < 7:
             flash('Для редагування даних введіть пароль два рази.', category='error')
+        elif User.query.filter_by(email=email).first():
+            flash('Користувач з такою поштою вже існує!', category='error')
+            return redirect(url_for('users.home'))
         else:
             current_user.login = login
             current_user.email = email
